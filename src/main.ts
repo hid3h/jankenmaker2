04bb48f3dc938bccb94dbe796c039b737e622beb
hid3h <razyetts@hotmail.co.jp>
2023-06-10 14:46:35 +0900

    layoutを使うように

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { join } from "path";
 import { NestExpressApplication } from "@nestjs/platform-express";
+import * as hbs from "hbs";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -9,6 +10,10 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, "..", "public"));
   app.setBaseViewsDir(join(__dirname, "..", "views"));
   app.setViewEngine("hbs");
+
+  // partialsの登録
+  hbs.registerPartials(join(__dirname, "..", "views", "layouts"));
+  hbs.registerPartials(join(__dirname, "..", "views", "partials"));
 
   await app.listen(8080);
 }
