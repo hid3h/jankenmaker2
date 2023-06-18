f9ac7ffe43f63bb6332e507f100eeb9e0fe233ce
hid3h <razyetts@hotmail.co.jp>
2023-06-18 12:25:29 +0900

    coolpoko対応

--- src/jankens/jankens.controller.ts
+++ src/jankens/jankens.controller.ts
@@ -3,7 +3,9 @@ import {
   Controller,
   Get,
   NotFoundException,
+  Param,
   Post,
+  Query,
   Render,
 } from "@nestjs/common";
 import { JankensService } from "./jankens.service";
@@ -13,40 +15,42 @@ import { JankenPlayingDto } from "./dto/janken-playing-dto";
 export class JankensController {
   constructor(private readonly jankenService: JankensService) {}
 
-  @Get("hamtaro")
+  @Get("j/:id")
   @Render("jankens/show")
-  show() {
-    const id = "hamtaro";
+  show(@Param("id") id: string, @Query("r") result: string | undefined) {
     const janken = this.jankenService.find(id);
     if (!janken) {
       // 404 Not Found
       throw new NotFoundException();
     }
 
+    const ogpIamgePath = this.jankenService.getOgpImagePath(janken, result);
+
     return {
       id: janken.id,
       title: janken.title,
-      imagePath: janken.playerWinImagePath,
+      imagePath: janken.beforePlayingImagePath,
       ogp: {
         title: janken.title,
-        description: "タップしてハム太郎とじゃんけんをすることができます。",
-        url: "https://jankenmaker.com/hamtaro",
-        imageUrl: "https://jankenmaker.com/ham1.jpg",
+        description: janken.description,
+        url: `https://jankenmaker.com/j/${janken.id}`,
+        imageUrl: `https://jankenmaker.com${ogpIamgePath}`,
       },
+      formActionPath: `/j/${janken.id}`,
+      beforePlayingMessage: janken.beforePlayingMessage,
     };
   }
 
-  @Post("hamtaro")
+  @Post("j/:id")
   @Render("jankens/result")
-  create(@Body() jankenPlayingDto: JankenPlayingDto) {
-    const id = "hamtaro";
+  create(@Param("id") id: string, @Body() jankenPlayingDto: JankenPlayingDto) {
     const result = this.jankenService.play(id);
     return {
       id: result.janken.id,
       title: result.janken.title,
-      imagePath: result.janken.playerWinImagePath,
+      imagePath: result.imagePath,
       jankenResultText: result.jankenResultText,
-      message: result.message,
+      message: result.jankenResultDisplayMessage,
       palayerHand: jankenPlayingDto.hand,
       twitterShareUrl: result.twitterShareUrl,
     };
--- src/jankens/jankens.service.ts
+++ src/jankens/jankens.service.ts
@@ -2,47 +2,61 @@ import { Injectable, NotFoundException } from "@nestjs/common";
 
 export type JankenResult = "playerWin" | "playerLose" | "draw";
 
+type JankeResultDisplay = {
+  imagePath: string;
+  messages: string[];
+  twitterShareMessage: string;
+};
+
 type Janken = {
   id: string;
   title: string;
   description: string;
   winRate: number;
-  playerWinImagePath: string;
-  playerWinMessages: string[];
-  playerLoseImagePath: string;
-  playerLoseMessages: string[];
+  beforePlayingImagePath: string;
+  beforePlayingMessage: string;
+  playerWin: JankeResultDisplay;
+  playerLose: JankeResultDisplay;
   twitterHashtag: string;
 };
 
 const jankens: Record<string, Janken> = {
-  hamtaro: {
-    id: "hamtaro",
-    title: "ハム太郎とじゃんけん",
-    description: "ハム太郎とじゃんけんをすることができます。",
-    winRate: 0.99,
-    playerWinImagePath: "ham1.jpg",
-    playerWinMessages: [
-      "やるやん！\n明日は俺にリベンジさせて。\nじゃあ、どうぞなのだ",
-    ],
-    playerLoseImagePath: "ham1.jpg",
-    playerLoseMessages: [
-      "ぼくの勝ちなのだ！\n負けは次に繋がるチャンスなのだ。\nネバーギブアップ！\nほな、いただきます。\n一日一回勝負。\nそれじゃあ、また明日なのだ。",
-      "ぼくの勝ちなのだ！\nたかがじゃんけん、そう思ってないですか？\nそれやったら明日も俺が勝ちますよ。\nほな、いただきます。\n一日一回勝負。\nそれじゃあ、また明日なのだ。",
-      "ぼくの勝ちなのだ！\n何で負けたか、明日まで考えといてなのだ。\nそしたら何かが見えてくるはずです。\nほな、いただきます。\n一日一回勝負。\nそれじゃあ、また明日なのだ。",
-    ],
-    twitterHashtag: "ハム太郎とじゃんけん",
-  },
+  // hamtaro: {
+  //   id: "hamtaro",
+  //   title: "ハム太郎とじゃんけん",
+  //   description: "ハム太郎とじゃんけんをすることができます。",
+  //   winRate: 0.99,
+  //   beforePlayingImagePath: "/ham1.jpg",
+  //   playerWinMessages: [
+  //     "やるやん！\n明日は俺にリベンジさせて。\nじゃあ、どうぞなのだ",
+  //   ],
+  //   afterPlayingImagePath: "/ham1.jpg",
+  //   playerLoseMessages: [
+  //     "ぼくの勝ちなのだ！\n負けは次に繋がるチャンスなのだ。\nネバーギブアップ！\nほな、いただきます。\n一日一回勝負。\nそれじゃあ、また明日なのだ。",
+  //     "ぼくの勝ちなのだ！\nたかがじゃんけん、そう思ってないですか？\nそれやったら明日も俺が勝ちますよ。\nほな、いただきます。\n一日一回勝負。\nそれじゃあ、また明日なのだ。",
+  //     "ぼくの勝ちなのだ！\n何で負けたか、明日まで考えといてなのだ。\nそしたら何かが見えてくるはずです。\nほな、いただきます。\n一日一回勝負。\nそれじゃあ、また明日なのだ。",
+  //   ],
+  //   twitterHashtag: "ハム太郎とじゃんけん",
+  //   beforePlayinMessage: "",
+  // },
   coolpoko: {
     id: "coolpoko",
     title: "クールポコとじゃんけん",
     description: "クールポコとじゃんけんをすることができます。",
-    winRate: 0.95,
-    playerWinImagePath: "poko-player-win.jpg",
-    playerWinMessages: ["なああああああああああああにいいいいいい"],
-    playerLoseImagePath: "poko-player-lose.jpg",
-    playerLoseMessages: [
-      "なああああああああああああにいいいいいい\nやっちまったなあ！",
-    ],
+    winRate: 0.9,
+    beforePlayingImagePath: "/poko-before-playing.jpeg",
+    beforePlayingMessage: "なああああああああああああ",
+    playerWin: {
+      imagePath: "/poko-player-win.jpeg",
+      messages: ["にいいいいいい\n勝っちまったなあ！"],
+      twitterShareMessage:
+        "なああああああああああああにいいいいいい\n勝っちまったなあ！",
+    },
+    playerLose: {
+      imagePath: "/poko-player-lose.jpeg",
+      messages: ["... ..."],
+      twitterShareMessage: "... ...",
+    },
     twitterHashtag: "クールポコとじゃんけん",
   },
 };
@@ -64,27 +78,66 @@ export class JankensService {
     const jankenResult: JankenResult =
       Math.random() < janken.winRate ? "playerLose" : "playerWin";
 
-    const message: string =
-      janken[jankenResult + "Messages"][
-        Math.floor(Math.random() * janken[jankenResult + "Messages"].length)
+    const jankenResultDisplay: JankeResultDisplay = janken[jankenResult];
+
+    const jankenResultDisplayMessage: string =
+      jankenResultDisplay.messages[
+        Math.floor(Math.random() * jankenResultDisplay.messages.length)
       ];
 
     const jankenResultText =
       jankenResult === "playerWin" ? "YOU WIN" : "YOU LOSE";
 
     const twitterSharedText = encodeURIComponent(
-      jankenResultText + "\n" + message,
+      jankenResultText + "\n" + jankenResultDisplay.twitterShareMessage,
     );
 
     const hashtag = encodeURIComponent(janken.twitterHashtag);
 
-    const twitterShareUrl = `https://twitter.com/share?url=https://jankenmaker.com/${jankenId}&hashtags=${hashtag}&text=${twitterSharedText}`;
+    const jankenResultParam = this.jankenResultParam(jankenResult);
+    const twitterShareUrl = `https://twitter.com/share?url=https://jankenmaker.com/j/${jankenId}?r=${jankenResultParam}&hashtags=${hashtag}&text=${twitterSharedText}`;
 
     return {
       janken,
       jankenResultText,
-      message,
+      jankenResultDisplayMessage,
+      imagePath: jankenResultDisplay.imagePath,
       twitterShareUrl,
     };
+  }
+
+  getOgpImagePath(janken: Janken, jankenResultParam?: string): string {
+    if (!jankenResultParam) {
+      return janken.beforePlayingImagePath;
+    }
+
+    const jankenResult = this.jankenResultFromParam(jankenResultParam);
+    const jankenResultDisplay: JankeResultDisplay = janken[jankenResult];
+    return jankenResultDisplay.imagePath;
+  }
+
+  private getRandomCharacter(from: string): string {
+    return from[Math.floor(Math.random() * from.length)];
+  }
+
+  private jankenResultParam(outcome: JankenResult): string {
+    const alphaNumericChars = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const firstChar =
+      outcome === "playerWin"
+        ? this.getRandomCharacter("abcdefghijklm")
+        : this.getRandomCharacter("nopqrstuvwxyz");
+
+    let remainingString = "";
+    for (let i = 0; i < 4; i++) {
+      remainingString += this.getRandomCharacter(alphaNumericChars);
+    }
+
+    return firstChar + remainingString;
+  }
+
+  jankenResultFromParam(randomString: string): JankenResult {
+    return "abcdefghijklm".includes(randomString[0])
+      ? "playerWin"
+      : "playerLose";
   }
 }
