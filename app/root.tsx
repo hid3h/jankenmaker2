0fa906836afef51c6d54d284e361b0be21658be6
hid3h <razyetts@hotmail.co.jp>
2024-04-06 10:29:59 +0900

    tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { LinksFunction } from "@remix-run/node";
 import {
   Links,
   Meta,
@@ -5,6 +6,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import stylesheet from "~/tailwind.css?url";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -27,3 +29,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
   return <Outlet />;
 }
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,6 +10,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <h1>Welcome to Remix deploy3</h1>
       <ul>
         <li>
